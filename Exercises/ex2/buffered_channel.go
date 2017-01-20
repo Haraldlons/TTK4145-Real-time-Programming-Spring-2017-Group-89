bcb60c32b3ccb1990e3c78fadea53eabeb6843ad
Petter <pettehw@stud.ntnu.no>
2017-01-20 11:55:17 +0100

    Code for go ex. 2

---
+++ Exercises/ex2/buffered_channel.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	. "fmt"
+	"runtime"
+	"sync"
+	//"time"
+)
+
+func thread1(i chan int, wg *sync.WaitGroup) {
+	for j := 0; j < 1000000-1; j++ {
+		local_i := <-i
+		local_i++
+		i <- local_i
+	}
+	wg.Done()
+}
+
+func thread2(i chan int, wg *sync.WaitGroup) {
+	for j := 0; j < 1000000; j++ {
+		local_i := <-i
+		local_i--
+		i <- local_i
+	}
+	wg.Done()
+}
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	var wg sync.WaitGroup
+
+	i := make(chan int, 2)
+	i <- 0
+
+	wg.Add(1)
+	go thread1(i, &wg)
+	wg.Add(1)
+	go thread2(i, &wg)
+
+	//time.Sleep(700 * time.Millisecond)
+	wg.Wait()
+	Println(<-i)
+}
