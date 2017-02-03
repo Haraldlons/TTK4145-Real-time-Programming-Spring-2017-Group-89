bef909a175a7c68df21aee85a499264d6e255bf3
Petter <pettehw@stud.ntnu.no>
2017-02-03 09:12:41 +0100

    Added /project and basic const definitions

---
+++ project/src/definitions/definitions.go
@@ -0,0 +1,16 @@
+package definitions
+
+import "fmt"
+
+const (
+	//General constants
+	N_ELEVS          int  = 3
+	N_FLOORS         int  = 4
+	BUTTON_CALL_UP   int  = 0
+	BUTTON_CALL_DOWN int  = 1
+	DIR_UP           int  = 1
+	DIR_DOWN         int  = -1
+	DIR_STOP         int  = 0
+	ON               bool = 1
+	OFF              bool = 0
+)
