6ce07b5e2c5c6131855ebfcac0d0a83ef75b91e6
Petter <pettehw@stud.ntnu.no>
2017-02-17 11:24:21 +0100

    Added basic file read/write functionality. Cleaned up main.go

--- project/main.go
+++ project/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	//"controller"
 	//"master"
-	"./src/driver"
+	//"./src/buttons"
+	//"./src/definitions"
+	//"./src/driver"
+	"./src/storage"
+	//"./src/master"
+	//"./src/watchdog"
+
 	//"./src/network"
 	"fmt"
 	//"os"
@@ -11,31 +16,7 @@ import (
 )
 
 func main() {
-	fmt.Println("Test")
-	//network.Run()
-
-	driver.Elev_init();
-	driver.Elev_set_motor_direction(driver.DIRECTION_DOWN)
-
-	//driver.Elev_set_motor_direction(driver.DIRECTION_UP);
-
-	for true {
-		fmt.Println(driver.Elev_get_floor_sensor_signal())
-		fmt.Println(driver.DIRECTION_DOWN)
-
-		//driver.Elev_set_motor_direction(driver.DIRECTION_DOWN)
-
-		if driver.Elev_get_floor_sensor_signal() == driver.N_FLOORS - 1 {
-			fmt.Println("Bobby Brown")
-			driver.Elev_set_motor_direction(driver.DIRECTION_DOWN)
-		} else if driver.Elev_get_floor_sensor_signal() == 0{
-			fmt.Println("Bobby Brown inverse")
-			driver.Elev_set_motor_direction(driver.DIRECTION_UP)
-		}
-
-		if driver.Elev_get_stop_signal() != 0 {
-			driver.Elev_set_motor_direction(driver.DIRECTION_STOP)
-			return
-		}
-	}
+	fmt.Printf("Test av lagring\n")
+	storage.StoreInternalButtonPresses()
+	storage.LoadInternalButtonPresses()
 }
--- project/src/elevator/elevator.go
+++ project/src/elevator/elevator.go
@@ -2,7 +2,36 @@ package elevator
 
 import (
 	"fmt"
+	"driver"
 )
+
+func test() {
+	//Test of elevator drivers
+	driver.Elev_init()
+	driver.Elev_set_motor_direction(driver.DIRECTION_DOWN)
+
+	//driver.Elev_set_motor_direction(driver.DIRECTION_UP);
+
+	for true {
+		fmt.Println(driver.Elev_get_floor_sensor_signal())
+		fmt.Println(driver.DIRECTION_DOWN)
+
+		//driver.Elev_set_motor_direction(driver.DIRECTION_DOWN)
+
+		if driver.Elev_get_floor_sensor_signal() == driver.N_FLOORS-1 {
+			fmt.Println("Bobby Brown")
+			driver.Elev_set_motor_direction(driver.DIRECTION_DOWN)
+		} else if driver.Elev_get_floor_sensor_signal() == 0 {
+			fmt.Println("Bobby Brown inverse")
+			driver.Elev_set_motor_direction(driver.DIRECTION_UP)
+		}
+
+		if driver.Elev_get_stop_signal() != 0 {
+			driver.Elev_set_motor_direction(driver.DIRECTION_STOP)
+			return
+		}
+	}
+}
 
 func ExecuteOrders(orders struct) bool {
 	return true
