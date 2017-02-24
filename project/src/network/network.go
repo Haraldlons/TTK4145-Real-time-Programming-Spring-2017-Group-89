0358a611a5288f61deb1b13a6d4eec2e6dd33f25
sveihaug@gmail.com <sveihaug@gmail.com>
2017-02-24 20:40:43 +0100

    Added 'backup' so when the program resumes after kill it finishes last order. Also tried to implement network as a crazy man with alot of random code-snippets. Don't even try to understand it

--- project/src/network/network.go
+++ project/src/network/network.go
@@ -1,57 +1,58 @@
 package network
 
-import (
-	//"bufio"
-	"log"
-	"net"
-	//"os"
-	"time"
-	//"strconv"
-)
+// import (
+// 	//"bufio"
+// 	"log"
+// 	"net"
+// 	//"os"
+// 	"time"
+// 	//"strconv"
+// )
 
-var listenTimer = 100 * time.Millisecond
-var broadcastTimer = 100 * time.Millisecond
+// var listenTimer = 100 * time.Millisecond
+// var broadcastTimer = 100 * time.Millisecond
 
-func check(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
+// func check(err error) {
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
 
-// Function for checking arguments passed to network functions
-func checkArgs() {
+// // Function for checking arguments passed to network functions
+// func checkArgs() {
 
-}
+// }
 
-func Broadcast(port string, bcAddr string, sendChan chan string) {
-	addr, err := net.ResolveUDPAddr("udp", bcAddr+port)
-	check(err)
+// func Broadcast(port string, bcAddr string, sendChan chan string) {
+// 	addr, err := net.ResolveUDPAddr("udp", bcAddr+port)
+// 	check(err)
 
-	conn, err := net.DialUDP("udp", nil, addr)
-	check(err)
+// 	conn, err := net.DialUDP("udp", nil, addr)
+// 	check(err)
 
-	log.Println("Broadcasting connection established on: ", bcAddr+port)
+// 	log.Println("Broadcasting connection established on: ", bcAddr+port)
 
-	msg := make([]byte, 1024)
-	for {
-		conn.Write(msg)
-		time.Sleep(broadcastTimer)
-	}
-}
+// 	msg := make([]byte, 1024)
+// 	for {
+// 		conn.Write(msg)
+// 		time.Sleep(broadcastTimer)
+// 	}
+// }
 
-func Listen(port string, listenChan chan string) {
-	addr, err := net.ResolveUDPAddr("udp", port)
-	check(err)
+// func Listen(port string, listenChan chan string) {
+// 	addr, err := net.ResolveUDPAddr("udp", port)
+// 	check(err)
 
-	conn, err := net.ListenUDP("udp", addr)
-	check(err)
+// 	conn, err := net.ListenUDP("udp", addr)
+// 	check(err)
 
-	log.Println("Listening connection established on port ", port)
+// 	log.Println("Listening connection established on port ", port)
 
-	buf := make([]byte, 1024)
-	for {
-		conn.ReadFromUDP(buf)
-		listenChan <- string(buf) // Dunno
-		time.Sleep(listenTimer)
-	}
-}
+// 	buf := make([]byte, 1024)
+// 	for {
+// 		conn.ReadFromUDP(buf)
+// 		listenChan <- string(buf) // Dunno
+// 		time.Sleep(listenTimer)
+// 	}
+// }
+/*Above is the original code before my expermintal coding-session*/
