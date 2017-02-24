def13081c5aba53651637be129c60e96546179f6
Petter <pettehw@stud.ntnu.no>
2017-02-24 09:47:28 +0100

    updated ex6

--- Exercises/ex6/main.go
+++ Exercises/ex6/main.go
@@ -2,16 +2,60 @@ package main
 
 import (
 	"fmt"
+	"net"
 )
 
-func slave() {
+var address string = "localhost"
+var bcAdress string = "129.241.187.255"
+var port string = "20020"
+
+func check(err error){
+	if err != nil {
+		panic(err)
+	}
+}
+
+func slave(conn net.UDPConn, isAlive bool) bool {
+
+	fmt.Println("Mufasa is dead. Long live the king.")
+}
+
+func master(startCount int, udpBroadcast *net.UDPconn) {
+
+
+	msg := make([]byte, 1)
+	count++
+	fmt.Println(*count)
+}
+
+func sendAlive(){
 
 }
 
-func master() {
+func listenAlive(){
+
+}
+
+func changeMaster(){
 
 }
 
 func main() {
+	fmt.Println("test")
+	isAlive bool := false
+	count := 0
 
+	udpAddr, err := net.ResolveUDPAddr("udp", port)
+	check(err)
+
+	udpListen, err := net.ListenUDP("udp", udpAddr)
+	check(err)
+
+	udpBroadcast, err := net.DialUDP("udp", nil, udpAddr)
+	check(err)
+
+	fmt.Println("Run primary")
+	master(count, udpBroadcast)
+
+	defer udpBroadcast.close
 }
