d0d6593c17fba66dd7db8f3fb89683c14dc1c6b8
Petter <pettehw@stud.ntnu.no>
2017-02-24 14:09:41 +0100

    stuff

--- project/src/elevator/elevator.go
+++ project/src/elevator/elevator.go
@@ -5,36 +5,40 @@ import (
 	"driver"
 )
 
-func test() {
-	//Test of elevator drivers
-	driver.Elev_init()
-	driver.Elev_set_motor_direction(driver.DIRECTION_DOWN)
-
-	//driver.Elev_set_motor_direction(driver.DIRECTION_UP);
-
-	for true {
-		fmt.Println(driver.Elev_get_floor_sensor_signal())
-		fmt.Println(driver.DIRECTION_DOWN)
-
-		//driver.Elev_set_motor_direction(driver.DIRECTION_DOWN)
-
-		if driver.Elev_get_floor_sensor_signal() == driver.N_FLOORS-1 {
-			fmt.Println("Bobby Brown")
-			driver.Elev_set_motor_direction(driver.DIRECTION_DOWN)
-		} else if driver.Elev_get_floor_sensor_signal() == 0 {
-			fmt.Println("Bobby Brown inverse")
-			driver.Elev_set_motor_direction(driver.DIRECTION_UP)
-		}
-
-		if driver.Elev_get_stop_signal() != 0 {
-			driver.Elev_set_motor_direction(driver.DIRECTION_STOP)
-			return
-		}
+func ExecuteOrders(orders struct, elevatorState *definitions.ElevatorState) bool {
+	for _, elem := range orders {
+		goToFloor(elem.destinationFloor, elevatorState)
 	}
 }
 
-func ExecuteOrders(orders struct) bool {
-	return true
+func goToFloor(destinationFloor int, elevatorState *definitions.ElevatorState) bool {
+	fmt.Println("Going to floor: ", destinationFloor)
+	lastFloor := elevatorState.LastFloor
+
+	if(driver.Elev_get_floor_sensor_signal() == destinationFloor){
+		fmt.Println("You are already on the desired floor")
+		driver.Elev_set_motor_direction(driver.DIRECTION_STOP)
+		return true
+	} 
+	else {  /*You are not on the desired floor*/
+		if lastFloor < destinationFloor {
+			driver.Elev_set_motor_direction(driver.DIRECTION_UP)
+		} else {
+			driver.Elev_set_motor_direction(driver.DIRECTION_DOWN)
+		}
+		for {
+			if driver.Elev_get_floor_sensor_signal() == destinationFloor {
+				fmt.Println("You reached your desired floor. Walk out")
+				driver.Elev_set_floor_indicator(destinationFloor) // Turn on corresponding floor indicator
+				driver.Elev_set_motor_direction(driver.DIRECTION_STOP) // Stop elevator
+				return true
+			}else {
+				time.Sleep(delay)
+			}
+		}
+	}
+
+
 }
 
 func GetState() struct {
--- project/src/network/network.go
+++ project/src/network/network.go
@@ -2,39 +2,56 @@ package network
 
 import (
 	//"bufio"
-	"fmt"
+	"log"
 	"net"
-	"os"
+	//"os"
+	"time"
 	//"strconv"
 )
 
-func checkError(err error) {
+var listenTimer = 100 * time.Millisecond
+var broadcastTimer = 100 * time.Millisecond
+
+func check(err error) {
 	if err != nil {
-		fmt.Println("Error: ", err)
-		os.Exit(0)
+		panic(err)
 	}
 }
 
-//var baddr *net.UDPAddr
+// Function for checking arguments passed to network functions
+func checkArgs() {
 
-func Test() {
-	//Prepare address at any address at port 20020
-	serverAddr, err := net.ResolveUDPAddr("udp4", ":20020")
-	checkError(err)
+}
 
-	//Listen at selected port
-	serverConn, err := net.ListenUDP("udp4", serverAddr)
-	checkError(err)
-	defer serverConn.Close()
+func Broadcast(port string, bcAddr string, sendChan chan string) {
+	addr, err := net.ResolveUDPAddr("udp", bcAddr+port)
+	check(err)
+
+	conn, err := net.DialUDP("udp", nil, addr)
+	check(err)
+
+	log.Println("Broadcasting connection established on: ", bcAddr+port)
+
+	msg := make([]byte, 1024)
+	for {
+		conn.Write(msg)
+		time.Sleep(broadcastTimer)
+	}
+}
+
+func Listen(port string, listenChan chan string) {
+	addr, err := net.ResolveUDPAddr("udp", port)
+	check(err)
+
+	conn, err := net.ListenUDP("udp", addr)
+	check(err)
+
+	log.Println("Listening connection established on port ", port)
 
 	buf := make([]byte, 1024)
-
 	for {
-		n, addr, err := serverConn.ReadFromUDP(buf)
-		fmt.Println("Received ", string(buf[0:n]), " from ", addr)
-
-		if err != nil {
-			fmt.Println("Error: ", err)
-		}
+		conn.ReadFromUDP(buf)
+		listenChan <- string(buf) // Dunno
+		time.Sleep(listenTimer)
 	}
 }
