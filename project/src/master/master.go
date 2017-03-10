2920d67b53c31f0c23b6ea72e1a9c39789314e87
Petter <pettehw@stud.ntnu.no>
2017-03-10 11:54:58 +0100

    Stuff in master

--- project/src/master/master.go
+++ project/src/master/master.go
@@ -41,3 +41,51 @@ func findClosestElevator(order definitions.Order, elevatorStates [definitions.N_
 	}
 	return closest
 }
+
+// Returns int corresponding to elevator with lowest cost (0:N_ELEV-1)
+func findLowestCostElevator(elevatorStates [definitions.N_ELEVS]definitions.ElevatorState, destinationFloor int) int{
+	cost := definitions.N_FLOORS
+	bestElevator := 0
+	for i:= 0; i < definitions.N_ELEVS; i++ { 
+		direction := findDirection(elevatorStates[i].LastFloor, destinationFloor)
+
+		if elevatorStates[i].Destination == definitions.IDLE { // Elevator is idle
+			tempCost := math.Abs(destinationFloor - elevatorStates[i].LastFloor)
+			if tempCost <= cost { //prioritize idle elevators
+				cost = tempCost
+				bestElevator = i
+			}
+
+		} else if direction == elevatorStates[i].Direction { // Elevator is moving in the correct direction
+			tempCost := math.Abs(destinationFloor - elevatorStates[i].LastFloor)
+			if tempCost < cost {
+				cost = tempCost
+				bestElevator = i
+			}
+
+		} else { // Elevator already passed the destination, or is moving in the wrong direction
+			tempCost := math.Abs(destinationFloor - elevatorStates[i].LastFloor) + math.Abs(elevatorStates[i].Destination-elevatorStates[i].LastFloor)
+			if tempCost < cost {
+				cost = tempCost
+				bestElevator = i
+			}
+
+		}
+	}
+	return bestElevator
+}
+
+func findDirection(lastFloor int, destinationFloor int) int {
+	if destinationFloor > lastFloor {
+		return definitions.DIR_UP
+	else if destinationFloor == lastFloor
+		return definitions.DIR_STOP
+	else
+		return definitions.DIR_DOWN
+	}
+}
+
+func UpdateOrders(orders interface {}) {
+	
+	orders.Orders[i] = 
+}
