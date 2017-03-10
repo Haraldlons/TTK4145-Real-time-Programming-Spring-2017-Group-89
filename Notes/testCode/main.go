53d29abfd0d2bfd37b4f591ab20a7e9e923bf4b5
Haraldlons <haraldlons@gmail.com>
2017-03-10 13:58:19 +0100

    New folder for testing

---
+++ Notes/testCode/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	// "./src/controller"
+	// "./src/definitions"
+	// "./src/driver"
+	// "./src/elevator"
+	// "./src/network"
+	//"./src/buttons"
+	//"./src/driver"
+	// "./src/storage"
+	//"./src/master"
+	//"./src/watchdog"
+	"fmt"
+	// "log"
+	// "os"
+	// "time"
+	// "fmt"
+	// "os/exec"
+)
+
+
+func main() {
+	fmt.Println("Main function started")
+
+	
+	return
+} //End main
